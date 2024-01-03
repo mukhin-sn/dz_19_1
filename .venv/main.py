0bebee54a3f94a25ef5b7d22fc0317776a8e33c7
Sergey Mukhin <senikomu@gmail.com>
2024-01-03 23:02:44 +0700

    Add main.py

---
+++ .venv/main.py
@@ -0,0 +1,44 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs, urlparse
+
+# Для начала определим настройки запуска
+hostName = "localhost" # Адрес для доступа по сети
+serverPort = 8080 # Порт для доступа по сети
+
+class MyServer(BaseHTTPRequestHandler):
+    """ 
+        Специальный класс, который отвечает за 
+        обработку входящих запросов от клиентов
+    """
+    def __get_html_content(self):
+        with open("index.html", "r", encoding="utf-8") as file:
+            self.file_data = file.read()
+        return self.file_data
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        query_components = parse_qs(urlparse(self.path).query)
+        page_content = self.__get_html_content()
+        print(query_components)
+        # print(page_content)
+        self.send_response(200) # Отправка кода ответа
+        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
+        self.end_headers() # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(page_content, "utf-8")) # Тело ответа
+
+if __name__ == "__main__":        
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети 
+    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print(f"Server started http://{hostName}:{serverPort}")
+
+    try:
+        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        pass
+
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    webServer.server_close()
+    print("Server stopped.")
